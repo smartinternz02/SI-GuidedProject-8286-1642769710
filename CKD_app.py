8b54946dc7c02bbde3c00d104f637916819c032f
tarun <tarunreddy163@gmail.com>
2022-01-25 12:52:00 +0530

    updated

--- CKD_app.py
+++ CKD_app.py
@@ -8,32 +8,25 @@ model = pickle.load(open('CKD.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-@app.route('/Prediction',methods=['POST','GET'])
-def prediction():
-    return render_template('ckd.html')
-@app.route('/Home',methods=['POST','GET'])
-def my_home():
     return render_template('ckd.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST','GET'])
 def predict():
     input_features = [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
 
-    features_name = ['blood_urea','blood_glucose_random','coronary_artery_disease',
-    'anemia','pus_cell','red_blood_cells','diabetesmellitus','pedal_edema']
+    features_name = ['red_blood_cells','pus_cell','diabetesmellitus','pedal_edema','anemia','coronary_artery_disease','blood_glucose_random','blood_urea']
 
     df = pd.DataFrame(features_value, columns=features_name)
 
     output = model.predict(df)
+    print(output)
+   
 
-    return render_template('result.html',predict_text=output)
-
-    if output==str(1):
-         return render_template('ckd.html',pred='You have CHRONIC KIDNEY DISEASE')
+    if output==1:
+         return render_template('ckd.html',prediction_text='You have CHRONIC KIDNEY DISEASE')
     else:
-        return render_template('ckd.html',pred='You do not have CHRONIC KIDNEY DISEASE')
+        return render_template('ckd.html',prediction_text='You do not have CHRONIC KIDNEY DISEASE')
 
 if __name__ == '__main__':
     app.run(debug=True)
